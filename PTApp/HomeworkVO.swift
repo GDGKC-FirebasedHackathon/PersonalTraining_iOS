98e2c42d423729a90f18527e083c8aa40a2cda99
OhKanghoon <OhKanghoon@oganghunui-MacBook-Pro.local>
2017-02-17 22:54:32 +0900

    TrainingVO 추가

--- PTApp/HomeworkVO.swift
+++ PTApp/HomeworkVO.swift
@@ -1,23 +1,23 @@
 //
-//  Diet.swift
+//  HomeworkVO.swift
 //  PTApp
 //
 //  Created by OhKanghoon on 2017. 2. 17..
 //  Copyright © 2017년 GDGKC-FirebasedHackathon. All rights reserved.
 //
 import Foundation
-class DietVO{
+
+class HomeworkVO{
     
-    init(id:String?, date:Date?,meals:[MealVO],trainer_id:String?,customer_id:String?){
+    init(id:Int?,date:Date?,trainer_id:String?,customer_id:String?){
         self.id = id
         self.date = date
-        self.meals = meals
         self.trainer_id = trainer_id
         self.customer_id = customer_id
     }
-    var id:String?
+    
+    var id:Int?
     var date:Date?
-    var meals = [MealVO]()
     var trainer_id:String?
     var customer_id:String?
 }
