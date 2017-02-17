89d1f1a462ce7af104204d04bf8757ae7005b693
YuJungin <Jungin@YJI.local>
2017-02-17 18:40:23 +0900

    식단 탭 화면 구성, tableview code 주석 추가

---
+++ PTApp/SecondMain_DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  SecondMain_DetailVC.swift
+//  PTApp
+//
+//  Created by YuJungin on 2017. 2. 17..
+//  Copyright © 2017년 GDGKC-FirebasedHackathon. All rights reserved.
+//
+
+import UIKit
+
+class SecondMain_DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+}
