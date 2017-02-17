a64ba9bbb310620669e443d69eea3bd8ea850c94
pumpa <redgun524@naver.com>
2017-02-17 19:37:31 +0900

    second.storyboard init 설정 ExtensionControl 키보드 내리는 메소드 추가

--- PTApp/ExtentionControl.swift
+++ PTApp/ExtentionControl.swift
@@ -31,4 +31,13 @@ extension UIViewController{
         }
     }
     
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
