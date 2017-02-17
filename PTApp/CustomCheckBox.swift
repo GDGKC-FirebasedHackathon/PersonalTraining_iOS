000d1f4a2a85cb2392003345cebccacf99ff5598
YuJungin <Jungin@YJI.local>
2017-02-18 00:39:29 +0900

    CustomCheckBox.swift 추

---
+++ PTApp/CustomCheckBox.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCheckBox.swift
+//  PTApp
+//
+//  Created by YuJungin on 2017. 2. 18..
+//  Copyright © 2017년 GDGKC-FirebasedHackathon. All rights reserved.
+//
+
+import UIKit
+class CustomCheckBox : UIButton{
+    var temp : Int?
+    
+}
