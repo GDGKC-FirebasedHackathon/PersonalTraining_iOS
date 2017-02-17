6dbcc464347bab940b8acf5e25482d74d891d5af
OhKanghoon <OhKanghoon@oganghunui-MacBook-Pro.local>
2017-02-18 00:03:17 +0900

    HomeworkVO에 motions추가

--- PTApp/HomeworkVO.swift
+++ PTApp/HomeworkVO.swift
@@ -9,15 +9,17 @@ import Foundation
 
 class HomeworkVO{
     
-    init(id:Int?,date:Date?,trainer_id:String?,customer_id:String?){
+    init(id:Int?,date:Date?,trainer_id:String?,customer_id:String?,motions:[MotionVO]){
         self.id = id
         self.date = date
+        self.motions = motions
         self.trainer_id = trainer_id
         self.customer_id = customer_id
     }
     
     var id:Int?
     var date:Date?
+    var motions:[MotionVO]
     var trainer_id:String?
     var customer_id:String?
 }
